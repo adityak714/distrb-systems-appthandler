b4164bbaa48646fb3e8259309f5d3815eddc7cd1
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-12-07 14:58:20 +0100

    fix: fix date time bug

--- src/Infrastructure/Repositories/appointmentRepository.ts
+++ src/Infrastructure/Repositories/appointmentRepository.ts
@@ -1,11 +1,19 @@
+/* eslint-disable prettier/prettier */
 import {IAppointment} from '../../Domain/Intefaces/IAppointment';
 import Appointment from '../Models/appointmentSchema';
 import {IAppointmentRepository} from '../../Domain/Intefaces/IAppointmentRepository';
 
 export class appointmentRepository implements IAppointmentRepository {
   async registerAppointment(newAppointment: IAppointment): Promise<void> {
-    newAppointment.date.setMinutes(0).toString();
-    newAppointment.date.setSeconds(0);
-    await Appointment.create(newAppointment).toString();
+    if (newAppointment.date.getMinutes() >= 0 && newAppointment.date.getMinutes() <= 29) {
+      newAppointment.date.setMinutes(0);
+      newAppointment.date.setSeconds(0);
+    }
+    else if (newAppointment.date.getMinutes() >= 30 && newAppointment.date.getMinutes() <= 59) {
+      newAppointment.date.setMinutes(30);
+      newAppointment.date.setSeconds(0);
+    }
+    console.log(newAppointment.date);
+    await Appointment.create(newAppointment);
   }
 }
