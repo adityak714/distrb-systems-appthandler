a1aeb294b7980d9efc6dac66bf009c0776734337
Aleksey Zorin <aleksey@student.chalmers.se>
2022-12-14 23:25:58 +0100

    Formatting for Nodemailer and some pictures

--- src/Infrastructure/Notifications/emailService.js
+++ src/Infrastructure/Notifications/emailService.js
@@ -3,9 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable prettier/prettier */
 const nodemailer_1 = __importDefault(require("nodemailer"));
 // const nodemailer = require('nodemailer'):
-async function main() {
+async function mailConfirmation() {
     await nodemailer_1.default.createTestAccount();
     const transporter = nodemailer_1.default.createTransport({
         host: 'smtp.office365.com',
@@ -21,10 +22,10 @@ async function main() {
     await transporter.sendMail({
         from: '"Lexa" <dentistimo11@outlook.com',
         to: 'alekseyzorin22@gmail.com',
-        subject: 'Fix your teeth',
-        text: 'Hello?',
-        html: '<b>Hello?</b>',
+        subject: 'Your Booking Confirmation',
+        text: 'Hello firstName! This if your confirmation for the appointemnt you booked with Dentistimo. Your time is: Date, time, dentistry',
+        html: '<img src="../../Domain/Assets/image.png" alt="Logo"> <br> <b>Hello firstName!</b> <br> This is your confirmation for the appointment you booked with Dentistimo. Your time is: <br> <i> Date<br> Time<br> Dentistry </i>',
     });
     console.log('Message sent: %s');
 }
-main().catch(console.error);
+mailConfirmation().catch(console.error);
--- src/Infrastructure/Notifications/emailService.ts
+++ src/Infrastructure/Notifications/emailService.ts
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 import nodemailer from 'nodemailer';
 // const nodemailer = require('nodemailer'):
-async function execute() {
+async function mailConfirmation() {
   await nodemailer.createTestAccount();
   const transporter = nodemailer.createTransport({
     host: 'smtp.office365.com',
@@ -16,11 +17,11 @@ async function execute() {
   await transporter.sendMail({
     from: '"Lexa" <dentistimo11@outlook.com',
     to: 'alekseyzorin22@gmail.com',
-    subject: 'Fix your teeth',
-    text: 'Hello?',
-    html: '<b>Hello?</b>',
+    subject: 'Your Booking Confirmation',
+    text: 'Hello firstName! This if your confirmation for the appointemnt you booked with Dentistimo. Your time is: Date, time, dentistry', // Plain text version of the message.
+    html: '<img src="../../Domain/Assets/image.png" alt="Logo"> <br> <b>Hello firstName!</b> <br> This is your confirmation for the appointment you booked with Dentistimo. Your time is: <br> <i> Date<br> Time<br> Dentistry </i>',
   });
   console.log('Message sent: %s');
 }
 
-execute().catch(console.error);
+mailConfirmation().catch(console.error);
