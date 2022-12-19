98b42a2bffda43426c90a21600f33f52d7921a45
Aleksey Zorin <aleksey@student.chalmers.se>
2022-12-19 20:56:00 +0100

    Separate methods for Nodemailer.

--- src/Infrastructure/Notifications/emailService.js
+++ src/Infrastructure/Notifications/emailService.js
@@ -1,12 +1,14 @@
 "use strict";
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-useless-escape */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable prettier/prettier */
 const nodemailer_1 = __importDefault(require("nodemailer"));
 // const nodemailer = require('nodemailer'):
-async function mailConfirmation() {
+// Booking confirmation email, sent when a user books an appointment.
+async function mailBookingConfirmation( /*recepient, dentistry, timeslot, date, dentist*/) {
     await nodemailer_1.default.createTestAccount();
     const transporter = nodemailer_1.default.createTransport({
         host: 'smtp.office365.com',
@@ -20,12 +22,81 @@ async function mailConfirmation() {
         logger: true,
     });
     await transporter.sendMail({
-        from: '"Lexa" <dentistimo11@outlook.com',
+        from: '"Dentistimo Team" <dentistimo11@outlook.com',
         to: 'alekseyzorin22@gmail.com',
         subject: 'Your Booking Confirmation',
-        text: 'Hello firstName! This if your confirmation for the appointemnt you booked with Dentistimo. Your time is: Date, time, dentistry',
-        html: '<img src="../../Domain/Assets/image.png" alt="Logo"> <br> <b>Hello firstName!</b> <br> This is your confirmation for the appointment you booked with Dentistimo. Your time is: <br> <i> Date<br> Time<br> Dentistry </i>',
+        text: 'Confirmation of booking!',
+        html: '',
+        attachments: [
+            {
+                filename: 'mailLogo.png',
+                path: '../../Domain/Assets/mailLogo.png',
+                cid: 'mailLogo'
+            }
+        ]
     });
     console.log('Message sent: %s');
 }
-mailConfirmation().catch(console.error);
+mailBookingConfirmation( /*recepient, dentistry, timeslot, date, dentist*/).catch(console.error);
+// Booking change email, sent when a user changes an appointment.
+async function mailBookingChange( /*recepient, dentistry, timeslot, date, dentist*/) {
+    await nodemailer_1.default.createTestAccount();
+    const transporter = nodemailer_1.default.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: 'dentistimo11@outlook.com',
+            pass: 'Myrslok2022',
+        },
+        logger: true,
+    });
+    await transporter.sendMail({
+        from: '"Dentistimo Team" <dentistimo11@outlook.com',
+        to: 'alekseyzorin22@gmail.com',
+        subject: 'Your Booking Confirmation',
+        text: 'You have changed your booking.',
+        html: '',
+        attachments: [
+            {
+                filename: 'mailLogo.png',
+                path: '../../Domain/Assets/mailLogo.png',
+                cid: 'mailLogo'
+            }
+        ]
+    });
+    console.log('Message sent: %s');
+}
+mailBookingChange( /*recepient, dentistry, timeslot, date, dentist*/).catch(console.error);
+// Booking deletion email, sent when a user deletes an appointment.
+async function mailBookingDeletion( /*recepient, dentistry, timeslot, date, dentist*/) {
+    await nodemailer_1.default.createTestAccount();
+    const transporter = nodemailer_1.default.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: 'dentistimo11@outlook.com',
+            pass: 'Myrslok2022',
+        },
+        logger: true,
+    });
+    await transporter.sendMail({
+        from: '"Dentistimo Team" <dentistimo11@outlook.com',
+        to: 'alekseyzorin22@gmail.com',
+        subject: 'Your Booking Confirmation',
+        text: 'You have deleted your booking.',
+        html: '',
+        attachments: [
+            {
+                filename: 'mailLogo.png',
+                path: '../../Domain/Assets/mailLogo.png',
+                cid: 'mailLogo'
+            }
+        ]
+    });
+    console.log('Message sent: %s');
+}
+mailBookingDeletion( /*recepient, dentistry, timeslot, date, dentist*/).catch(console.error);
--- src/Infrastructure/Notifications/emailService.ts
+++ src/Infrastructure/Notifications/emailService.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable no-useless-escape */
+
 import nodemailer from 'nodemailer';
+import handlebars from 'handlebars';
+import fs from 'fs';
 // const nodemailer = require('nodemailer'):
-async function mailConfirmation() {
+
+// Booking confirmation email, sent when a user books an appointment.
+async function mailBookingConfirmation(/*recepient, dentistry, timeslot, date, dentist*/) {
   await nodemailer.createTestAccount();
   const transporter = nodemailer.createTransport({
     host: 'smtp.office365.com',
@@ -15,13 +21,86 @@ async function mailConfirmation() {
     logger: true,
   });
   await transporter.sendMail({
-    from: '"Lexa" <dentistimo11@outlook.com',
+    from: '"Dentistimo Team" <dentistimo11@outlook.com',
     to: 'alekseyzorin22@gmail.com',
     subject: 'Your Booking Confirmation',
-    text: 'Hello firstName! This if your confirmation for the appointemnt you booked with Dentistimo. Your time is: Date, time, dentistry', // Plain text version of the message.
-    html: '<img src="../../Domain/Assets/image.png" alt="Logo"> <br> <b>Hello firstName!</b> <br> This is your confirmation for the appointment you booked with Dentistimo. Your time is: <br> <i> Date<br> Time<br> Dentistry </i>',
+    text: 'Confirmation of booking!', // Plain text version of the message left empty because of HTML template below.
+    html:
+    '' ,
+    attachments: [
+      {
+        filename: 'mailLogo.png',
+        path: '../../Domain/Assets/mailLogo.png',
+        cid: 'mailLogo'
+      }
+    ]
   });
   console.log('Message sent: %s');
 }
+mailBookingConfirmation(/*recepient, dentistry, timeslot, date, dentist*/).catch(console.error);
 
-mailConfirmation().catch(console.error);
+// Booking change email, sent when a user changes an appointment.
+async function mailBookingChange(/*recepient, dentistry, timeslot, date, dentist*/) {
+  await nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: 'dentistimo11@outlook.com',
+      pass: 'Myrslok2022',
+    },
+    logger: true,
+  });
+  await transporter.sendMail({
+    from: '"Dentistimo Team" <dentistimo11@outlook.com',
+    to: 'alekseyzorin22@gmail.com',
+    subject: 'Your Booking Confirmation',
+    text: 'You have changed your booking.', // Plain text version of the message left empty because of HTML template below.
+    html:
+    '' ,
+    attachments: [
+      {
+        filename: 'mailLogo.png',
+        path: '../../Domain/Assets/mailLogo.png',
+        cid: 'mailLogo'
+      }
+    ]
+  });
+  console.log('Message sent: %s');
+}
+mailBookingChange(/*recepient, dentistry, timeslot, date, dentist*/).catch(console.error);
+
+// Booking deletion email, sent when a user deletes an appointment.
+async function mailBookingDeletion(/*recepient, dentistry, timeslot, date, dentist*/) {
+  await nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: 'dentistimo11@outlook.com',
+      pass: 'Myrslok2022',
+    },
+    logger: true,
+  });
+  await transporter.sendMail({
+    from: '"Dentistimo Team" <dentistimo11@outlook.com',
+    to: 'alekseyzorin22@gmail.com',
+    subject: 'Your Booking Confirmation',
+    text: 'You have deleted your booking.', // Plain text version of the message left empty because of HTML template below.
+    html:
+    '' ,
+    attachments: [
+      {
+        filename: 'mailLogo.png',
+        path: '../../Domain/Assets/mailLogo.png',
+        cid: 'mailLogo'
+      }
+    ]
+  });
+  console.log('Message sent: %s');
+}
+mailBookingDeletion(/*recepient, dentistry, timeslot, date, dentist*/).catch(console.error);
