1eeaeaed97d9d1a6042699ec2291d00f13551cb3
Aditya Khadkikar <guskhadad@student.gu.se>
2022-12-23 19:53:09 +0100

    fix: time conversion to 24 hr instead of 12 hr

--- src/Domain/Utils/dateUtils.js
+++ src/Domain/Utils/dateUtils.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertToLocalTime = void 0;
 function convertToLocalTime(date, timeZone) {
-    return new Date(date).toLocaleString();
+    return new Date(date).toLocaleString(timeZone, { hour12: false });
 }
 exports.convertToLocalTime = convertToLocalTime;
