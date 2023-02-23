b202ecd390db360de1b2865d5d523c1c67200d29
Aditya Khadkikar <guskhadad@student.gu.se>
2023-02-23 05:16:40 +0100

    rm executable files

--- src/Domain/Utils/dateUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertToLocalTime = void 0;
-function convertToLocalTime(date, timeZone) {
-    return new Date(date).toLocaleString(timeZone, { hour12: false });
-}
-exports.convertToLocalTime = convertToLocalTime;
